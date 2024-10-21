eb8deb9fa934d4374a5374102f76c6292d8f6402
josephdeborah <josephdeborah873@gmail.com>
2024-10-21 12:55:22 -0700

    The rgb is not defined so i defined it. rgb is not defined before assigning it to another variable (color). I declared the rgb before assigning it to the variable.

---
+++ variable.js
@@ -0,0 +1,11 @@
+// rgb is not defined so i defined it
+ let rgb = ['Red', 'Green', 'Blue']
+let color = rgb;
+
+console.log(color)
+
+// Ensure this shows all
+
+
+// Ensure this get printed into the console
+console.log(rgb)
